5b8ddf88c12c75540676d1655290cbd1638979e2
Farhan <farhanisawake@gmail.com>
2026-07-25 16:15:37 +0600

    Refactor image URLs to use local paths instead of external links across multiple components and pages

--- src/data/updates.ts
+++ src/data/updates.ts
@@ -13,9 +13,7 @@ export const updatesData: Record<string, UpdateItem> = {
     tag: "International Achievement",
     title: "Our Raihan shines bright with the Silver Medal at the 67th IMO!",
     date: "Date: 20-07-2026",
-    images: [
-      "https://raw.githubusercontent.com/ccpactechnical-cloud/ccpac-official/main/silver.jpeg",
-    ],
+    images: ["/images/silver.jpeg"],
     content: `The Chittagong College Physics and Astronomy Club (CCPAC) is filled with immense pride and joy to share that, at the 67th International Mathematical Olympiad (IMO) 2026 held in China, Chittagong College student Md. Raihan Siddiquee has clinched a Silver Medal, scoring 23 out of 42. After earning an Honorable Mention at the 66th IMO in Sunshine Coast, Australia, last year, he has reached new heights of success.`,
   },
   "2": {
@@ -24,11 +22,7 @@ export const updatesData: Record<string, UpdateItem> = {
     title:
       "Tokens of Honour Conferred upon Honorable Principal, Vice Principal, and Moderator",
     date: "Date: 29-03-2026",
-    images: [
-      "https://raw.githubusercontent.com/ccpactechnical-cloud/ccpac-official/main/h1.jpeg",
-      "https://raw.githubusercontent.com/ccpactechnical-cloud/ccpac-official/main/h2.jpeg",
-      "https://raw.githubusercontent.com/ccpactechnical-cloud/ccpac-official/main/h3.jpeg",
-    ],
+    images: ["/images/h1.jpeg", "/images/h2.jpeg", "/images/h3.jpeg"],
     content: `Today, on behalf of the Executive Panel 2025–26 of Chattogram College Physics & Astronomy Club (CCPAC), tokens of honour were conferred upon Principal Prof. Mohammad Mozahidul Islam Chowdhury, Vice Principal Md. Shawkat Iqbal Faruqueee, and Moderator Md. Zahirul Islam in sincere recognition of their unwavering support.`,
   },
   "3": {
@@ -37,9 +31,7 @@ export const updatesData: Record<string, UpdateItem> = {
     title:
       "Congratulating Md. Shawkat Iqbal Faruqueee on his appointment as Vice Principal",
     date: "Date: 11-01-2026",
-    images: [
-      "https://raw.githubusercontent.com/ccpactechnical-cloud/ccpac-official/main/vp.jpeg",
-    ],
+    images: ["/images/vp.jpeg"],
     content: `Today, Chittagong College Physics & Astronomy Club warmly congratulated Md. Shawkat Iqbal Faruqueee on his appointment as the new Vice Principal of Chittagong College.`,
   },
 };
