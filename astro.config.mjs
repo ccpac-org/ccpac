b8cc56876e7f6e3b8ba4eb010d690fc81830df35
Farhan <farhanisawake@gmail.com>
2026-07-25 17:22:36 +0600

    feat: add Vercel adapter and update MembershipModal styles

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,16 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'http://ccpac.vercel.app'
+  site: "http://ccpac.vercel.app",
+  adapter: vercel({
+    webAnalytics: {
+      enabled: true,
+    },
+    maxDuration: 8,
+  }),
 });
