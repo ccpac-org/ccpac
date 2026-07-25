a03a93ed456f1b374dc7a4249ff8c74602961ae9
Farhan <farhanisawake@gmail.com>
2026-07-25 14:51:46 +0600

    Refactor code style and formatting across multiple files for consistency

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from "astro/config";
+import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   build: {
-    format: 'file'
-  }
+    format: "file",
+  },
 });
